6c30f089e82b1cb83d1c759a02545e0a442f60ca
Tiago Carção <tiago.carcao@gmail.com>
2025-10-06 12:26:16 +0100

    feat: Support commonjs as well

---
+++ bindings/node/index.cjs
@@ -0,0 +1,24 @@
+const path = require("node:path");
+const fs = require("node:fs");
+
+const root = path.join(__dirname, "..", "..");
+
+const binding = typeof process.versions.bun === "string"
+  // Support `bun build --compile` by being statically analyzable enough to find the .node file at build-time
+  ? require(`${root}/prebuilds/${process.platform}-${process.arch}/tree-sitter-craft.node`)
+  : require("node-gyp-build")(root);
+
+try {
+  const nodeTypesPath = path.join(root, "src", "node-types.json");
+  if (fs.existsSync(nodeTypesPath)) {
+    const nodeTypes = JSON.parse(fs.readFileSync(nodeTypesPath, "utf8"));
+    binding.nodeTypeInfo = nodeTypes;
+  }
+} catch (_) {}
+
+// Named exports for compatibility
+module.exports = binding;
+module.exports.language = binding;
+module.exports.nodeTypes = binding.nodeTypeInfo;
+module.exports.name = "tree-sitter-craft";
+module.exports.default = binding;
